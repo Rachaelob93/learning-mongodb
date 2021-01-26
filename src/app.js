881d2454f183ef3c7e30375ce9d0087b6ec0d491
Rachael O'Brien <rachaelob93@gmail.com>
2021-01-26 17:01:48 +0000

    use CRUD and add new data to database though use of models and schemas

---
+++ src/app.js
@@ -0,0 +1,29 @@
+require("./db/connection");
+const {Person} = require ("./models/Person");
+const {Task} = require ("./models/Task");
+
+
+const app = async () => {
+//     const person = new Person({
+//         name: "Rachael",
+//         email: "rachael@rachael.com",
+//         age: 27,
+//         likesPizza: true,
+//     });
+
+//     await person.save();
+
+//     const task = new Task({
+//         task: "buy painkillers",
+//         completed: false,
+//     });
+
+// await task.save();
+
+// const everything = await Person.find({})
+// console.log(everything[0].name);
+
+// await Task.deleteOne({task: "buy painkillers" })
+};
+
+app();
---
+++ src/models/Person.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email")
+            }
+        }
+    },
+    age:{
+        type: Number,   
+    },
+    likesPizza:{
+        type: Boolean,
+    },
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+module.exports = {
+    Person,
+}
